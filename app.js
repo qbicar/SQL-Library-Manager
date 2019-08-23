92bb70f85b431f17cb8e8bec95ec8ebcd142565c
qbicar <51241113+qbicar@users.noreply.github.com>
2019-08-23 15:59:07 -0400

    refact

--- app.js
+++ app.js
@@ -1,114 +1,89 @@
 const express = require('express');
 const Sequelize = require('sequelize');
 const path = require('path');
-const Books = require('./models/book.js');
+const Books = require('./models').Books;
 const app = express();
 const port = 3000;
+// const library = require('./library.db');
+
 //dialect specifies the specific version of sql im using
 //storage key to specify the path for sqlite and create a database called library
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: 'data/library.db',
-    logging: false, //disables log in terminal
-});
+const sequelize = require('./models').sequelize;
 
-//Defing Library model
-//initialize the model
-module.exports = (sequelize) => {
-    class Books extends Sequelize.Model { }
-    //defines a new table in the database with the name Library. Seq looks for information in that table
-    //string identifies that the variable will be set to up to  255 characters and interger is a number variable
-    Books.init({
-        id: {
-            type: Sequelize.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        title: {
-            type: Sequelize.STRING,
-            allowNull: false, //disallow Null
-            validate: {
-                notNull: {
-                    msg: 'Please provide a value for "Title"',
-                },
-                notEmpty: {
-                    msg: 'Please provide a value for "Title"',
-                }
-            },
-        },
-        author: {
-            type: Sequelize.STRING,
-            allowNull: false,
-            validate: {
-                notNull: {
-                    msg: 'Please provide a value for "Author"',
-                },
-                notEmpty: {
-                    msg: 'Please provide a value for "Author"',
-                }
-            },
-        },
-        genre: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
-        year: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-        },
-    },
-        {
-            freezeTableName: true,
-            sequelize
-        },
-        { sequelize });
-    return Books;
-}
-
-//IIFE(Immediately Invoked Function Expression) is a JavaScript function that runs as soon as it is define
-//authenticate returns a promise that resolves to a successful connection to database
-//force sync library table and logging the data returned by library.toJson();
-(async () => {
-    await sequelize.sync({ force: true });
-    try {
-        const books1 = await Books.create({
-            title: 'Chicka Chicka Boom Boom',
-            author: ' Bill Martin, Jr',
-            genre: 'Children',
-            year: 1989
-        });
-        console.log(books1.toJSON());
-        // await books.save();
-        const books2 = await Books.create({
-            title: 'Red Fish Blue Fish',
-            author: ' Dr.Seuss',
-            genre: 'Children',
-            year: 1960
-        });
-        console.log(books2.toJSON());
-        //converts to json
-        const books3 = await Books.build({
+sequelize.sync()
+    .then(
+        Books.findAll()
+            .then((books) => {
+                console.log(books)
+            })
+    )
+    .then(() => {
+        Books.create({
             title: 'where the wild things are',
             author: 'Maurice Sendak',
             genre: 'Children',
             year: 1963
-        });
-        await books3.save();
-        console.log(books3.toJSON());
-        // findByPk() (or 'find by primary key') retrieves a single instance by its primary key 
-        const bookById = await Books.findAll({
-            attributes: ['id', 'title'],
-        });
-        console.log(bookById.toJSON());
-    } catch (error) {
-        if (error.name === 'SequelizeValidationError') {
-            const errors = error.errors.map(err => err.message);
-            console.error('Validation errors:', errors)
-        } else {
-            throw error;
-        }
+        })
     }
-})();
+    )
+    .catch((error) => {
+        console.log(error)
+    })
+
+
+// Dummy.drop().then(() => {
+//     console.log('table deleted');
+// }).finally(() => {
+//     sequelize.close();
+// });
+
+//Defing Library model
+//initialize the model
+
+//IIFE(Immediately Invoked Function Expression) is a JavaScript function that runs as soon as it is define
+//authenticate returns a promise that resolves to a successful connection to database
+//force sync library table and logging the data returned by library.toJson();
+// (async () => {
+//     await sequelize.sync({ force: true });
+//     try {
+//         const books1 = await Books.create({
+//             title: 'Chicka Chicka Boom Boom',
+//             author: ' Bill Martin, Jr',
+//             genre: 'Children',
+//             year: 1989
+//         });
+//         console.log(books1.toJSON());
+//         // await books.save();
+//         const books2 = await Books.create({
+//             title: 'Red Fish Blue Fish',
+//             author: ' Dr.Seuss',
+//             genre: 'Children',
+//             year: 1960
+//         });
+//         console.log(books2.toJSON());
+//         //converts to json
+//         const books3 = await Books.build({
+//             title: 'where the wild things are',
+//             author: 'Maurice Sendak',
+//             genre: 'Children',
+//             year: 1963
+//         });
+//         await books3.save();
+//         console.log(books3.toJSON());
+//         // findByPk() (or 'find by primary key') retrieves a single instance by its primary key 
+//         const bookById = await Books.findAll({
+//             attributes: ['id', 'title'],
+//         });
+//         console.log(bookById.toJSON());
+//     } catch (error) {
+//         if (error.name === 'SequelizeValidationError') {
+//             const errors = error.errors.map(err => err.message);
+//             console.error('Validation errors:', errors)
+//         } else {
+//             throw error;
+//         }
+//     }
+// })();
 
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, "views"))
--- models/book.js
+++ models/book.js
@@ -1,12 +1,12 @@
 // 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Book = sequelize.define('Book', {
+  const Book = sequelize.define('Books', {
     title: DataTypes.STRING,
     author: DataTypes.STRING,
     genre: DataTypes.STRING,
     year: DataTypes.INTEGER
   }, {});
-  Book.associate = function(models) {
+  Book.associate = function (models) {
     // associations can be defined here
   };
   return Book;
