a6477bba06809cf9f32c9a2e23cd0d2f942d3305
qbicar <51241113+qbicar@users.noreply.github.com>
2019-08-26 15:56:34 -0400

    mostly works

--- app.js
+++ app.js
@@ -1,138 +1,87 @@
 const express = require('express');
-// const Sequelize = require('sequelize');
 const path = require('path');
-const Books = require('./models').Books;
+const Book = require('./models').Book;
 const app = express();
 const port = 3000;
-// const library = require('./library.db');
-
-//dialect specifies the specific version of sql im using
-//storage key to specify the path for sqlite and create a database called library
 const sequelize = require('./models').sequelize;
-
-sequelize.sync()
-    .then(() => {
-        Books.findAll()
-            .then((books) => {
-                console.log(books)
-            })
-    }
-    )
-    .then(() => {
-        Books.create({
-            title: 'where the wild things are',
-            author: 'Maurice Sendak',
-            genre: 'Children',
-            year: 1963
-        })
-    }
-    )
-    .catch((error) => {
-        console.log(error)
-    })
-
-
-// Dummy.drop().then(() => {
-//     console.log('table deleted');
-// }).finally(() => {
-//     sequelize.close();
-// });
-
-//Defing Library model
-//initialize the model
-
-//IIFE(Immediately Invoked Function Expression) is a JavaScript function that runs as soon as it is define
-//authenticate returns a promise that resolves to a successful connection to database
-//force sync library table and logging the data returned by library.toJson();
-// (async () => {
-//     await sequelize.sync({ force: true });
-//     try {
-//         const books1 = await Books.create({
-//             title: 'Chicka Chicka Boom Boom',
-//             author: ' Bill Martin, Jr',
-//             genre: 'Children',
-//             year: 1989
-//         });
-//         console.log(books1.toJSON());
-//         // await books.save();
-//         const books2 = await Books.create({
-//             title: 'Red Fish Blue Fish',
-//             author: ' Dr.Seuss',
-//             genre: 'Children',
-//             year: 1960
-//         });
-//         console.log(books2.toJSON());
-//         //converts to json
-//         const books3 = await Books.build({
-//             title: 'where the wild things are',
-//             author: 'Maurice Sendak',
-//             genre: 'Children',
-//             year: 1963
-//         });
-//         await books3.save();
-//         console.log(books3.toJSON());
-//         // findByPk() (or 'find by primary key') retrieves a single instance by its primary key 
-//         const bookById = await Books.findAll({
-//             attributes: ['id', 'title'],
-//         });
-//         console.log(bookById.toJSON());
-//     } catch (error) {
-//         if (error.name === 'SequelizeValidationError') {
-//             const errors = error.errors.map(err => err.message);
-//             console.error('Validation errors:', errors)
-//         } else {
-//             throw error;
-//         }
-//     }
-// })();
-
+const bodyParser = require('body-parser');
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, "views"))
-app.use("/static", express.static(__dirname + '/public'))
+app.use('/static', express.static('public'))
+app.use(bodyParser.json());
 
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+//works
 app.get('/', (req, res) => {
-    res.render('index', { Books })
-    //res.render('index', { books})
+  res.redirect('/books');
+});
+//works
+app.get('/books', async (req, res) => {
+  try {
+    const books = await Book.findAll()
+    res.render('index', { books: books })
+  } catch (error) {
+    console.log("Please try again")
+    res.render("error", { error })
+  };
 });
 
-// app.get('/books', (req, res) => {
-//    // res.render('index', { books })
-// });
-
+//works
 app.get('/books/new', (req, res) => {
-    res.render('new-book')
+  res.render("new-book");
 });
 
-// UserRouter.route('/books/new').post(function (req, res) {
-//     const book = new Book()
-// }) 
+//input more data
+app.post('/books/new', async (req, res) => {
+  Book.create(req.body)
+    .then(book => {
+      res.redirect("/books", { book })
+    })
+    .catch(error => {
+      if (error.name === 'SequelizeValidationError') {
+        const errors = error.errors.map(err => err.message);
+        res.render("error")
+        console.error('Validation error:', errors)
+      } else {
+        throw error;
+      };
+    });
+  res.render('new-book')
+});
+//works
+app.get('/book/:id', async (req, res) => {
+  const book = await Book.findByPk(req.params.id);
+  res.render('update-book', { book });
+});
 
-// app.get('/books/:id', (req, res) => {
-//     //res.render('index', { books })
-// });
+//Works
+app.post('/book/:id', async (req, res) => {
+  const book = await Book.findByPk(req.params.id);
+  await book.update(req.body)
+  alert('Book has been Updated');
+  res.redirect('/book', { book })
+});
 
-// UserRouter.route('/books/:id').post(function (req, res) {
-//     //update book
-// }) 
+app.post('book/:id/delete', async (req, res) => {
+  const book = await Book.findByPk(req.params.id);
+  await book.destroy(req.body)
+})
 
-// UserRouter.route('/books/:id/delete').post(function (req, res) {
-//     //delete the book
-// }) 
+//works
+app.use((req, res, next) => {
+  const err = new Error("Oh No !An error has occured")
+  err.status = 404
+  next(err)
+})
+app.use((err, req, res, next) => {
+  res.locals.error = err
+  res.status(err.status)
+  res.render('error')
+});
 
-
-// app.use((req, res, next) => {
-//     const err = new Error("Oh No !An error has occured")
-//     err.status = 404
-//     next(err)
-// })
-// app.use((err, req, res, next) => {
-//     res.locals.error = err
-//     res.status(err.status)
-//     res.render('error')
-// })
-
-
-
-
-app.listen(port);
-console.log("This application is listening on port " + port)
+//works
+sequelize.sync().then(() => {
+  app.listen(port);
+  console.log("This application is listening on port " + port)
+});
--- models/book.js
+++ models/book.js
@@ -1,13 +1,43 @@
-// 'use strict';
+'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Book = sequelize.define('Books', {
-    title: DataTypes.STRING,
-    author: DataTypes.STRING,
-    genre: DataTypes.STRING,
-    year: DataTypes.INTEGER
+  const Book = sequelize.define('Book', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Please provide a value for 'Title'",
+        },
+        notEmpty: {
+          msg: "Please provide a value for 'Title'",
+        }
+      },
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Please provide a value for 'Author'",
+        },
+        notEmpty: {
+          msg: "Please provide a value for 'Author'",
+        }
+      },
+    },
+    genre: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   }, {});
-  Book.associate = function (models) {
-    // associations can be defined here
-  };
   return Book;
 };
