14ae089f5eb6aa2850b608d3365241c92198cbef
qbicar <51241113+qbicar@users.noreply.github.com>
2019-08-27 13:11:57 -0400

    DONE

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 3000;
 const sequelize = require('./models').sequelize;
 const bodyParser = require('body-parser');
+
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, "views"))
 app.use('/static', express.static('public'))
@@ -12,18 +13,19 @@ app.use(bodyParser.json());
 
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
+
 //works
 app.get('/', (req, res) => {
   res.redirect('/books');
 });
+
 //works
 app.get('/books', async (req, res) => {
   try {
     const books = await Book.findAll()
     res.render('index', { books: books })
   } catch (error) {
-    console.log("Please try again")
-    res.render("error", { error })
+    res.render("page-not-found", { error })
   };
 });
 
@@ -32,23 +34,29 @@ app.get('/books/new', (req, res) => {
   res.render("new-book");
 });
 
-//input more data
+//Works
 app.post('/books/new', async (req, res) => {
-  Book.create(req.body)
-    .then(book => {
-      res.redirect("/books", { book })
+  try {
+    const { title, author, genre, year } = req.body
+    await Book.create({
+      title,
+      author,
+      genre,
+      year
+    }).then(() => {
+      res.redirect("/books")
     })
-    .catch(error => {
-      if (error.name === 'SequelizeValidationError') {
-        const errors = error.errors.map(err => err.message);
-        res.render("error")
-        console.error('Validation error:', errors)
-      } else {
-        throw error;
-      };
-    });
-  res.render('new-book')
+  } catch (error) {
+    if (error.name === 'SequelizeValidationError') {
+      const errors = error.errors.map(err => err.message);
+      res.render("error")
+      console.error('Validation error:', errors)
+    } else {
+      throw error;
+    };
+  }
 });
+
 //works
 app.get('/book/:id', async (req, res) => {
   const book = await Book.findByPk(req.params.id);
@@ -59,25 +67,25 @@ app.get('/book/:id', async (req, res) => {
 app.post('/book/:id', async (req, res) => {
   const book = await Book.findByPk(req.params.id);
   await book.update(req.body)
-  alert('Book has been Updated');
-  res.redirect('/book', { book })
+  res.redirect('/books')
 });
 
-app.post('book/:id/delete', async (req, res) => {
-  const book = await Book.findByPk(req.params.id);
-  await book.destroy(req.body)
+app.post('/books/:id/delete', async (req, res) => {
+  const bookDestroy = await Book.findByPk(req.params.id);
+  await bookDestroy.destroy();
+  res.redirect('/books');
 })
 
 //works
 app.use((req, res, next) => {
   const err = new Error("Oh No !An error has occured")
-  err.status = 404
+  err.status = 404 || 500
   next(err)
 })
 app.use((err, req, res, next) => {
   res.locals.error = err
   res.status(err.status)
-  res.render('error')
+  res.render('page-not-found')
 });
 
 //works
